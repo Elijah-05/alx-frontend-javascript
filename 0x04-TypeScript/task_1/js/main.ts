6c77a37b359e81e0a998964f1711ad36b9dec3b9
Elijah <ellayab804@gmail.com>
2025-09-13 11:59:30 +0300

    print teacher function and function interface update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,45 +1,50 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
 const teacher1: Teacher = {
-    firstName: "John",
-    lastName: "Doe",
-    fullTimeEmployee: true,
-    location: "New York",
-    contract: false,
-    age: 30,
-    subject: "Mathematics"
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: true,
+  location: "New York",
+  contract: false,
+  age: 30,
+  subject: "Mathematics",
 };
 
 interface Director extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 }
 
 const director1: Director = {
-    firstName: "Jane",
-    lastName: "Smith",
-    fullTimeEmployee: true,
-    location: "Los Angeles",
-    numberOfReports: 5,
-    age: 40,
-    department: "Science"
+  firstName: "Jane",
+  lastName: "Smith",
+  fullTimeEmployee: true,
+  location: "Los Angeles",
+  numberOfReports: 5,
+  age: 40,
+  department: "Science",
 };
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-    return `${firstName.charAt(0)}. ${lastName}`;
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
 }
-
 
 console.log(teacher1);
 console.log(director1);
-console.log(printTeacher("Jane", "Smith"));
+console.log(printTeacher({ firstName: "Jane", lastName: "Smith" }));
