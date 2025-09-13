419110deed55f7b5b553824322e35d8d59632f46
Elijah <ellayab804@gmail.com>
2025-09-13 15:10:30 +0300

    string literal type and function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -67,3 +67,20 @@ function executeWork(employee: Director | Teacher): string {
 // Example usage
 console.log(executeWork(createEmployee(200)));   // Getting to work
 console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
+
+
+// Task 7
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return 'No class today';
+  }
+}
+
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
