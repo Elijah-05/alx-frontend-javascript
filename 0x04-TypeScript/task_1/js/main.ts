360b65694569142bb89f2af9f4ddd8c21cdcc25d
Elijah <ellayab804@gmail.com>
2025-09-13 13:01:02 +0300

    print teacher function interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,7 +35,7 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher (firstName: string, lastName: string) {
+function printTeacher (firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
 
